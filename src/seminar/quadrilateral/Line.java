14b80412584219c738c68a6432d6dcf07b73f68f
gerrit <gerrit.leuchner@gmail.com>
2017-05-01 22:48:50 +0200

    init

--- src/seminar/quadrilateral/Line.java
+++ src/seminar/quadrilateral/Line.java
@@ -8,7 +8,20 @@ public class Line {
 
 	public Line(Point2D a, Point2D b) {
 		points = new Point2D[2];
-		points[0] = a;
-		points[1] = b;
+		if (a.x() < b.x()) {
+			points[0] = a;
+			points[1] = b;
+		} else {
+			points[0] = b;
+			points[1] = a;
+		}
+	}
+
+	public double getSlope() {
+		return (points[1].y() - points[0].y()) / (points[1].x() - points[0].x());
+	}
+
+	public double getLength() {
+		return Math.sqrt(Math.pow(points[1].x() - points[0].x(), 2) + Math.pow(points[1].y() - points[0].y(), 2));
 	}
 }
--- src/seminar/quadrilateral/QuadrilateralChecker.java
+++ src/seminar/quadrilateral/QuadrilateralChecker.java
@@ -1,15 +1,18 @@
 package seminar.quadrilateral;
 
+import java.util.ArrayList;
+
+import edu.princeton.cs.algs4.GrahamScan;
 import edu.princeton.cs.algs4.Point2D;
 
 public class QuadrilateralChecker {
 
-	public final float EPS = 0.000001f;
+	public final float EPS = 0.0001f;
 
 	public String getType(Point2D[] points) {
 		String type = "NOT_A_QUADRILATERAL";
 		if (points.length == 4) {
-			Point2D[] hull = getConvexHull();
+			Point2D[] hull = getConvexHull(points);
 			if (hull.length < 4) {
 				type = "CONCAVE";
 			} else {
@@ -23,10 +26,13 @@ public class QuadrilateralChecker {
 
 				if (areParalell(lines[0], lines[2]) ^ areParalell(lines[1], lines[3])) {
 					type = "TRAPEZOID";
-				} else if (areParalell(lines[0], lines[2]) ^ areParalell(lines[1], lines[3])) {
+				} else if (areParalell(lines[0], lines[2]) & areParalell(lines[1], lines[3])) {
 					type = "PARALELLOGRAM";
-					if (formRightAngle(lines[0], lines[3]) && formRightAngle(lines[1], lines[2])) {
+					if (formRightAngle(lines[0], lines[3]) & formRightAngle(lines[1], lines[2])) {
 						type = "RECTANGLE";
+						if (compareFloatingPoint(lines[0].getLength(), lines[3].getLength())) {
+							type = "SQUARE";
+						}
 					}
 				}
 			}
@@ -35,14 +41,22 @@ public class QuadrilateralChecker {
 	}
 
 	private boolean formRightAngle(Line line, Line line2) {
-		return false;
+		return compareFloatingPoint(-1.0 / line.getSlope(), line2.getSlope());
 	}
 
 	private boolean areParalell(Line line, Line line2) {
-		return false;
+		return compareFloatingPoint(line.getSlope(), line2.getSlope());
 	}
 
-	private Point2D[] getConvexHull() {
-		return null;
+	private boolean compareFloatingPoint(double d, double d2) {
+		return Math.abs(d - d2) <= EPS;
+	}
+
+	private Point2D[] getConvexHull(Point2D[] points) {
+		GrahamScan grahamScan = new GrahamScan(points);
+		Iterable<Point2D> hull = grahamScan.hull();
+		ArrayList<Point2D> lst = new ArrayList<>();
+		hull.forEach(p -> lst.add(p));
+		return (Point2D[]) lst.toArray();
 	}
 }
